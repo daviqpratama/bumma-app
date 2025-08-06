049dfae8f0c3b155139de5e913f750e5b0162ec9
daviqpratama <daviqpratama28@gmail.com>
2025-08-06 21:35:27 +0700

    bisa pasti 1000%

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Paksa semua URL menggunakan HTTPS saat environment production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
