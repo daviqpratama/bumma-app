bb72d527eee44b58458e4cd5de49a0f212518ff9
daviqpratama <daviqpratama28@gmail.com>
2025-08-06 19:20:20 +0700

    update version php

--- database/migrations/2025_08_06_191058_create_pengumuman_table.php
+++ database/migrations/2025_08_06_191058_create_pengumuman_table.php
@@ -8,27 +8,22 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-       Schema::create('pengumuman', function (Blueprint $table) {
-    $table->id();
-    $table->string('judul');
-    $table->text('deskripsi');
-    $table->date('tanggal');
-    $table->timestamps();
-});
-
+        Schema::create('pengumuman', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('isi');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('pengumuman');
     }
